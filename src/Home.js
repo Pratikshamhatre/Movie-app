b95aae26526e397fdd01f794ac0db0f03eea76c1
Pratikshamhatre <mhatrepratiksha95@gmail.com>
2021-04-14 12:56:22 +0530

    Initial Commit

---
+++ src/Home.js
@@ -0,0 +1,18 @@
+import axios from 'axios';
+import React, { Component } from 'react';
+
+
+export default class Home extends Component {
+
+    componentDidMount(){
+const noewPlayingUrl="https://api.themoviedb.org/3/movie/now_playing";
+    }
+    render() {
+        return (
+            <div>
+            <h1>Home</h1>
+                
+            </div>
+        )
+    }
+}
